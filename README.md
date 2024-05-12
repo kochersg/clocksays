3762d3b014ce0500a31676564974ff03a860fe79
Gerrit Kocherscheidt <kochersg@koco-group.com>
2024-05-12 08:05:23 +0200

    English language support added

--- README.md
+++ README.md
@@ -1,3 +1,14 @@
 # clocksays
 
-Describe your project here.
+This small projects converts a given time (datetime.datetime) in a string of natural language. Languages supported so far:
+* German
+* English
+
+Feel free to add other languages and issue pull request.
+
+## Usage
+```python
+import clocksays.saytime as st
+import datetime as dt
+t_str = st.time2words(t = dt.datetime.now(), language='de')
+print(t_str)
--- src/clocksays/start.py
+++ src/clocksays/start.py
@@ -1,61 +1,64 @@
 import clocksays.saytime as st
 import datetime as dt
 
+
 def test():
     for m in range(60):
-        t = dt.datetime.strptime(f'2024-01-01 14:{m:02}:01','%Y-%m-%d %H:%M:%S')
-        print(t, st.time2words(t=t, language='de'))
-        
+        t = dt.datetime.strptime(f"2024-01-01 14:{m:02}:01", "%Y-%m-%d %H:%M:%S")
+        print(t, st.time2words(t=t, language="en"))
+
+
 def test2():
-    t = dt.datetime.strptime('2024-01-01 14:01:01','%Y-%m-%d %H:%M:%S')
+    t = dt.datetime.strptime("2024-01-01 14:01:01", "%Y-%m-%d %H:%M:%S")
     print(t, st.time2words(t=t))
-    t = dt.datetime.strptime('2024-01-01 11:16:01','%Y-%m-%d %H:%M:%S')
+    t = dt.datetime.strptime("2024-01-01 11:16:01", "%Y-%m-%d %H:%M:%S")
     print(t, st.time2words(t=t))
-    t = dt.datetime.strptime('2024-01-01 11:32:01','%Y-%m-%d %H:%M:%S')
+    t = dt.datetime.strptime("2024-01-01 11:32:01", "%Y-%m-%d %H:%M:%S")
     print(t, st.time2words(t=t))
-    t = dt.datetime.strptime('2024-01-01 11:46:01','%Y-%m-%d %H:%M:%S')
+    t = dt.datetime.strptime("2024-01-01 11:46:01", "%Y-%m-%d %H:%M:%S")
     print(t, st.time2words(t=t))
-    t = dt.datetime.strptime('2024-01-01 13:58:01','%Y-%m-%d %H:%M:%S')
+    t = dt.datetime.strptime("2024-01-01 13:58:01", "%Y-%m-%d %H:%M:%S")
     print(t, st.time2words(t=t))
-    t = dt.datetime.strptime('2024-01-01 23:14:01','%Y-%m-%d %H:%M:%S')
+    t = dt.datetime.strptime("2024-01-01 23:14:01", "%Y-%m-%d %H:%M:%S")
     print(t, st.time2words(t=t))
-    t = dt.datetime.strptime('2024-01-01 09:28:01','%Y-%m-%d %H:%M:%S')
+    t = dt.datetime.strptime("2024-01-01 09:28:01", "%Y-%m-%d %H:%M:%S")
     print(t, st.time2words(t=t))
-    t = dt.datetime.strptime('2024-01-01 02:44:01','%Y-%m-%d %H:%M:%S')
+    t = dt.datetime.strptime("2024-01-01 02:44:01", "%Y-%m-%d %H:%M:%S")
     print(t, st.time2words(t=t))
-    t = dt.datetime.strptime('2024-01-01 23:15:01','%Y-%m-%d %H:%M:%S')
+    t = dt.datetime.strptime("2024-01-01 23:15:01", "%Y-%m-%d %H:%M:%S")
     print(t, st.time2words(t=t))
-    t = dt.datetime.strptime('2024-01-01 09:30:01','%Y-%m-%d %H:%M:%S')
+    t = dt.datetime.strptime("2024-01-01 09:30:01", "%Y-%m-%d %H:%M:%S")
     print(t, st.time2words(t=t))
-    t = dt.datetime.strptime('2024-01-01 02:00:01','%Y-%m-%d %H:%M:%S')
+    t = dt.datetime.strptime("2024-01-01 02:00:01", "%Y-%m-%d %H:%M:%S")
     print(t, st.time2words(t=t))
-    t = dt.datetime.strptime('2024-01-01 07:45:01','%Y-%m-%d %H:%M:%S')
-    print(t, st.time2words(t=t))
-
-    t = dt.datetime.strptime('2024-01-01 07:12:01','%Y-%m-%d %H:%M:%S')
-    print(t, st.time2words(t=t))
-    t = dt.datetime.strptime('2024-01-01 07:20:01','%Y-%m-%d %H:%M:%S')
+    t = dt.datetime.strptime("2024-01-01 07:45:01", "%Y-%m-%d %H:%M:%S")
     print(t, st.time2words(t=t))
 
-    t = dt.datetime.strptime('2024-01-01 07:33:01','%Y-%m-%d %H:%M:%S')
+    t = dt.datetime.strptime("2024-01-01 07:12:01", "%Y-%m-%d %H:%M:%S")
     print(t, st.time2words(t=t))
-    t = dt.datetime.strptime('2024-01-01 09:38:01','%Y-%m-%d %H:%M:%S')
-    print(t, st.time2words(t=t))
-
-    t = dt.datetime.strptime('2024-01-01 07:20:01','%Y-%m-%d %H:%M:%S')
+    t = dt.datetime.strptime("2024-01-01 07:20:01", "%Y-%m-%d %H:%M:%S")
     print(t, st.time2words(t=t))
 
-    t = dt.datetime.strptime('2024-01-01 07:21:01','%Y-%m-%d %H:%M:%S')
+    t = dt.datetime.strptime("2024-01-01 07:33:01", "%Y-%m-%d %H:%M:%S")
+    print(t, st.time2words(t=t))
+    t = dt.datetime.strptime("2024-01-01 09:38:01", "%Y-%m-%d %H:%M:%S")
     print(t, st.time2words(t=t))
 
-    t = dt.datetime.strptime('2024-01-01 09:48:01','%Y-%m-%d %H:%M:%S')
+    t = dt.datetime.strptime("2024-01-01 07:20:01", "%Y-%m-%d %H:%M:%S")
     print(t, st.time2words(t=t))
-    t = dt.datetime.strptime('2024-01-01 09:52:01','%Y-%m-%d %H:%M:%S')
+
+    t = dt.datetime.strptime("2024-01-01 07:21:01", "%Y-%m-%d %H:%M:%S")
+    print(t, st.time2words(t=t))
+
+    t = dt.datetime.strptime("2024-01-01 09:48:01", "%Y-%m-%d %H:%M:%S")
+    print(t, st.time2words(t=t))
+    t = dt.datetime.strptime("2024-01-01 09:52:01", "%Y-%m-%d %H:%M:%S")
     print(t, st.time2words(t=t))
 
 
 def main():
     pass
 
-if __name__=="__main__":
+
+if __name__ == "__main__":
     main()
